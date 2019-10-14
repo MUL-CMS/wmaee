c2e07cd7f295b88156f010456d5e234086bc1150
dominik <dominik.noeger@unileoben.ac.at>
2019-10-14 17:07:26 +0200

    Forward import init __init__.py

--- wmaee/__init__.py
+++ wmaee/__init__.py
@@ -1,6 +1,7 @@
 # This is to hide moudle data
 # plotting functions
 from .wmaee import plot_projected_dos, plot_total_dos
+from plotly.graph_objects import Figure, Scatter, Contour
 #utility functions
 from .wmaee import view, ase_to_pymatgen, pymatgen_to_ase, working_directory
 
@@ -16,6 +17,9 @@ from ase.build import bulk
 from pymatgen import Structure, Lattice, Orbital, Spin
 from pymatgen.io.vasp import Incar, Kpoints, Poscar, Potcar, Outcar, Oszicar, Vasprun
 
+# Forward system functions, since it should be for dummies
+from os import getcwd as current_directory, listdir, walk, mkdir
+from os.path import isfile, isdir, join, exists
 
 # Greet from
 from .wmaee import greet as _greet
