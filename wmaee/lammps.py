a6501d7bd3cf071e8de088ad63a7b64fd90f4800
dnoeger <dominik.noeger@unileoben.ac.at>
2020-05-01 12:41:01 +0200

    Revised docstrings to ReSturcturedText aded GrandCanonical MonteCarlo wrapper

--- wmaee/lammps.py
+++ wmaee/lammps.py
@@ -1,6 +1,6 @@
 from wmaee.core.common import LoggerMixin
 from wmaee.utils import to_pyiron
-from typing import Union, Optional, List, Collection, NoReturn
+from typing import Union, Optional, Dict, Collection, NoReturn, List
 from ase import Atoms as AseAtoms
 from pyiron.atomistics.structure.atoms import Atoms as IronAtoms
 from pyiron.base.settings.generic import Settings
@@ -112,24 +112,89 @@ class LAMMPSInput(LoggerMixin):
     def md(self, temperature: Optional[None, float] = None, pressure: Optional[None, float] = None,
            n_ionic_steps: Optional[int] = 1000, time_step: Optional[float] = 1.0, n_print: Optional[int] = 100,
            temperature_damping_timescale: Optional[int] = 100.0, pressure_damping_timescale: Optional[float] = 1000.0,
-           seed: Optional[int, None] = None, tloop=None, initial_temperature: Optional[float, None] = None,
-           langevin: Optional[bool] = False, delta_temp: Optional[float, None] = None,
-           delta_press: Optional[float, None] = None):
+           seed: Optional[Union[int, None]] = None, tloop=None, initial_temperature: Optional[Union[float, None]] = None,
+           langevin: Optional[bool] = False, delta_temp: Optional[Union[float, None]] = None,
+           delta_press: Optional[Union[float, None]] = None):
         """
         Set an MD calculation within LAMMPS. Nosé Hoover is used by default.
 
-        :param temperature (None or float): Target temperature. If set to None, an NVE calculation is performed. It is required when the pressure is set or langevin is set
-        :param pressure (None or float): Target pressure. If set to None, an NVE or an NVT calculation is performed. (This tag will allow for a list in the future as it is done for calc_minimize())
-        :param n_ionic_steps (int): Number of ionic steps
-        :param time_step (float): Step size between two steps. In fs if units==metal
-        :param n_print (int):  Print frequency
-        :param temperature_damping_timescale (float): The time associated with the thermostat adjusting the temperature. (In fs. After rescaling to appropriate time units, is equivalent to Lammps' `Tdamp`.)
-        :param pressure_damping_timescale (float): The time associated with the barostat adjusting the temperature. (In fs. After rescaling to appropriate time units, is equivalent to Lammps' `Pdamp`.)
-        :param seed (int):  Seed for the random number generation (required for the velocity creation)
-        :param tloop:
-        :param initial_temperature (None or float):  Initial temperature according to which the initial velocity field is created. If None, the initial temperature will be twice the target temperature (which would go immediately down to the target temperature as described in equipartition theorem). If 0, the velocity field is not initialized (in which case  the initial velocity given in structure will be used). If any other number is given, this value is going to be used for the initial temperature.
-        :param langevin (bool): (True or False) Activate Langevin dynamics
-        :param delta_temp (float): Thermostat timescale, but in your Lammps time units, whatever those are. (DEPRECATED.)
-        :param delta_press (float): Barostat timescale, but in your Lammps time units, whatever those are. (DEPRECATED.)
+        :param temperature: (None or float) Target temperature. If set to None, an NVE calculation is performed. It is required when the pressure is set or langevin is set
+        :param pressure: (None or float) Target pressure. If set to None, an NVE or an NVT calculation is performed. (This tag will allow for a list in the future as it is done for calc_minimize())
+        :param n_ionic_steps: (int) Number of ionic steps
+        :param time_step: (float) Step size between two steps. In fs if units==metal
+        :param n_print: (int)  Print frequency
+        :param temperature_damping_timescale: (float) The time associated with the thermostat adjusting the temperature. (In fs. After rescaling to appropriate time units, is equivalent to Lammps' `Tdamp`.)
+        :param pressure_damping_timescale: (float) The time associated with the barostat adjusting the temperature. (In fs. After rescaling to appropriate time units, is equivalent to Lammps' `Pdamp`.)
+        :param seed: (int) Seed for the random number generation (required for the velocity creation)
+        :param tloop: TODO: Find out what this parameter is good for
+        :param initial_temperature: (None or float)  Initial temperature according to which the initial velocity field is created. If None, the initial temperature will be twice the target temperature (which would go immediately down to the target temperature as described in equipartition theorem). If 0, the velocity field is not initialized (in which case  the initial velocity given in structure will be used). If any other number is given, this value is going to be used for the initial temperature.
+        :param langevin: (bool) Activate Langevin dynamics
+        :param delta_temp: (float) Thermostat timescale, but in your Lammps time units, whatever those are. (DEPRECATED.)
+        :param delta_press: (float) Barostat timescale, but in your Lammps time units, whatever those are. (DEPRECATED.)
         """
         pass
+
+    def minimize(self, e_tol: Optional[float] = 0.0, f_tol: Optional[float] = 1e-4, max_iter: Optional[int] = 100000,
+                 pressure: Optional[Union[Collection, float]] = None, n_print: Optional[int] = 100,
+                 style: Optional[str] = 'cg'):
+        """
+        Sets parameters required for minimization.
+        :param: e_tol: (float) If the magnitude of difference between energies of two consecutive steps is lower than or equal to `e_tol`, the minimisation terminates. (Default is 0.0 eV.)
+        :param f_tol: (float) If the magnitude of the global force vector at a step is lower than or equal to `f_tol`, the minimisation terminates. (Default is 1e-4 eV/angstrom.)  max_iter (int): Maximum number of minimisation steps to carry out. If the minimisation converges before `max_iter` steps, terminate at the converged step. If the minimisation does not converge up to `max_iter` steps, terminate at the `max_iter` step. (Default is 100000.)
+        :param pressure: (float, list, tuple, numpy.ndarray) Pressure in GPa at which minimisation is to be carried out. If None, isochoric (constant volume) condition will be used. If a float, cell shape changes are allowed. in each of the three primary axes, but no shearing occurs. If array-like, (up to) the first six elements will be interpreted as the x, y, z, xy, xz, and yz components of the pressure tensor, respectively. If this component-wise mode is used, cell changes corresponding to any one element of the stress tensor can be selectively disabled by setting this element to None. (Default is None, run isochorically.)
+        :param n_print: (int) Write (dump or print) to the output file every n steps (Default: 100)
+        :param style: ('cg'/'sd'/other values from Lammps docs) The style of the numeric minimization, either conjugate gradient, steepest descent, or other keys permissible from the Lammps docs on 'min_style'. (Default is 'cg' -- conjugate gradient.)
+        """
+        pass
+
+    def vcsgc(self, mu: Dict,
+        ordered_element_list: List,
+        target_concentration: Optional[Union[Dict, None]]=None,
+        kappa: Optional[int]=1000.,
+        mc_step_interval: Optional[int]=100,
+        swap_fraction: Optional[float]=0.1,
+        temperature_mc: Optional[float, None]=None,
+        window_size: Optional[float, None]=None,
+        window_moves: Optional[int, None]=None,
+        temperature: Optional[Union[None, float]]=None,
+        pressure: Optional[Union[None, float]]=None,
+        n_ionic_steps: Optional[int]=1000,
+        time_step: Optional[float]=1.0,
+        n_print: Optional[int]=100,
+        temperature_damping_timescale: Optional[float]=100.0,
+        pressure_damping_timescale: Optional[float]=1000.0,
+        seed: Optional[Union[int, None]]=None,
+        initial_temperature: Optional[float, None]=None,
+        langevin: Optional[bool]=False):
+        """
+        Run variance-constrained semi-grand-canonical MD/MC for a binary system. In addition to VC-SGC arguments, all
+        arguments for a regular MD calculation are also accepted.
+        https://vcsgc-lammps.materialsmodeling.org
+
+        Note:
+            For easy visualization later (with `get_structure`), it is highly recommended that the initial structure
+            contain at least one atom of each species.
+
+        Warning:
+            Assumes the units are metal, otherwise units for the constraints may be off.
+
+        :param mu: (dict) A dictionary of chemical potentials, one for each element the potential treats, where the dictionary keys are just the chemical symbol. Note that only the *relative* chemical potentials are used here, such that the swap acceptance probability is influenced by the chemical potential difference between the two species (a more negative value increases the odds of swapping *to* that element.)
+        :param ordered_element_list: (list) A list of the chemical species symbols in the order they appear in the definition of the potential in the Lammps' input file.
+        :param target_concentration: (dict) A dictionary of target simulation domain concentrations for each species *in the potential*. Dictionary keys should be the chemical symbol of the corresponding species, and the sum of all concentrations must be 1. (Default is None, which runs regular semi-grand-canonical MD/MC without any variance constraint.)
+        :param kappa: (int) Variance constraint for the MC. Larger value means a tighter adherence to the target concentrations. (Default is 1000.)
+        :param mc_step_interval: (int) How many steps of MD between each set of MC moves. (Default is 100.) Must divide the number of ionic steps evenly.
+        :param swap_fraction: (float) The fraction of atoms whose species is swapped at each MC phase. (Default is 0.1.)
+        :param temperature_mc: (float) The temperature for accepting MC steps. (Default is None, which uses the MD temperature.)
+        :param window_size: (float) The size of the sampling window for parallel calculations as a fraction of something unspecified in the VC-SGC docs, but it must lie between 0.5 and 1. (Default is None, window is determined automatically.)
+        :param window_moves: (int) The number of times the sampling window is moved during one MC cycle. (Default is None, number of moves is determined automatically.)
+        :param temperature: (None or float) Target temperature. If set to None, an NVE calculation is performed. It is required when the pressure is set or langevin is set
+        :param pressure: (None or float) Target pressure. If set to None, an NVE or an NVT calculation is performed. (This tag will allow for a list in the future as it is done for calc_minimize())
+        :param n_ionic_steps: (int) Number of ionic steps
+        :param time_step: (float) Step size between two steps. In fs if units==metal
+        :param n_print: (int)  Print frequency
+        :param temperature_damping_timescale: (float) The time associated with the thermostat adjusting the temperature. (In fs. After rescaling to appropriate time units, is equivalent to Lammps' `Tdamp`.)
+        :param pressure_damping_timescale: (float) The time associated with the barostat adjusting the temperature. (In fs. After rescaling to appropriate time units, is equivalent to Lammps' `Pdamp`.)
+        :param seed: (int)  Seed for the random number generation (required for the velocity creation)
+        :param initial_temperature: (None or float)  Initial temperature according to which the initial velocity field is created. If None, the initial temperature will be twice the target temperature (which would go immediately down to the target temperature as described in equipartition theorem). If 0, the velocity field is not initialized (in which case  the initial velocity given in structure will be used). If any other number is given, this value is going to be used for the initial temperature.
+        :param langevin: (bool) (True or False) Activate Langevin dynamics
+        """
