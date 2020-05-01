50e65efd360a6f5dcb2f15775a2544afcf17aa93
dnoeger <dominik.noeger@unileoben.ac.at>
2020-05-01 12:16:30 +0200

    Adapted documentation from pyiron for md

--- wmaee/lammps.py
+++ wmaee/lammps.py
@@ -4,7 +4,6 @@ from typing import Union, Optional, List, Collection, NoReturn
 from ase import Atoms as AseAtoms
 from pyiron.atomistics.structure.atoms import Atoms as IronAtoms
 from pyiron.base.settings.generic import Settings
-from pyiron.lammps.lammps import LammpsInteractive
 from pyiron.project import Project
 from pymatgen import Structure
 from pandas import DataFrame
@@ -110,3 +109,27 @@ class LAMMPSInput(LoggerMixin):
                 str(list_of_elements),
             )
 
+    def md(self, temperature: Optional[None, float] = None, pressure: Optional[None, float] = None,
+           n_ionic_steps: Optional[int] = 1000, time_step: Optional[float] = 1.0, n_print: Optional[int] = 100,
+           temperature_damping_timescale: Optional[int] = 100.0, pressure_damping_timescale: Optional[float] = 1000.0,
+           seed: Optional[int, None] = None, tloop=None, initial_temperature: Optional[float, None] = None,
+           langevin: Optional[bool] = False, delta_temp: Optional[float, None] = None,
+           delta_press: Optional[float, None] = None):
+        """
+        Set an MD calculation within LAMMPS. Nosé Hoover is used by default.
+
+        :param temperature (None or float): Target temperature. If set to None, an NVE calculation is performed. It is required when the pressure is set or langevin is set
+        :param pressure (None or float): Target pressure. If set to None, an NVE or an NVT calculation is performed. (This tag will allow for a list in the future as it is done for calc_minimize())
+        :param n_ionic_steps (int): Number of ionic steps
+        :param time_step (float): Step size between two steps. In fs if units==metal
+        :param n_print (int):  Print frequency
+        :param temperature_damping_timescale (float): The time associated with the thermostat adjusting the temperature. (In fs. After rescaling to appropriate time units, is equivalent to Lammps' `Tdamp`.)
+        :param pressure_damping_timescale (float): The time associated with the barostat adjusting the temperature. (In fs. After rescaling to appropriate time units, is equivalent to Lammps' `Pdamp`.)
+        :param seed (int):  Seed for the random number generation (required for the velocity creation)
+        :param tloop:
+        :param initial_temperature (None or float):  Initial temperature according to which the initial velocity field is created. If None, the initial temperature will be twice the target temperature (which would go immediately down to the target temperature as described in equipartition theorem). If 0, the velocity field is not initialized (in which case  the initial velocity given in structure will be used). If any other number is given, this value is going to be used for the initial temperature.
+        :param langevin (bool): (True or False) Activate Langevin dynamics
+        :param delta_temp (float): Thermostat timescale, but in your Lammps time units, whatever those are. (DEPRECATED.)
+        :param delta_press (float): Barostat timescale, but in your Lammps time units, whatever those are. (DEPRECATED.)
+        """
+        pass
