423ba1027a82ab522d53ac30def9e205982a2f46
dnoeger <dominik.noeger@unileoben.ac.at>
2020-05-13 20:00:05 +0200

    Refactoring of extesions.unsuppported.triplets

---
+++ wmaee/extensions/unsupported/triplets/pairs.py
@@ -0,0 +1,26 @@
+import numpy as np
+from wmaee.extensions.unsupported.triplets.sampling import sampling_function, ArrayLike
+from wmaee.core.types import Atoms, Collection, Union, Optional, Tuple, Iterable
+
+
+def sample_pair_axes(start: float, cut: float, mu: float, num: Optional[int] = 50,
+                     pot: Tuple[float, float] = (5, 1)) -> ArrayLike:
+    """
+    Samples a half circle of triplets, from a minimum distance to the a maximum distance of "cut" and denser
+    sampling around "mu", where "pot" are the weights of a Mie-Type weighting function
+    :param start: (float) the minimum distance between the atoms
+    :param cut: (float) the cutoff radius
+    :param mu: (float) the expected minimum
+    :param num: (int) the number of samples on each axis
+    :param pot: (tuple of int) the powers of the Mie.Potential weighting function
+    :return: (np.ndarray) sample of the axis
+    """
+    x = np.linspace(start, cut, num=10000)
+    x[np.isclose(x, 0.0)] = 1e-10
+    samples = sampling_function(x, mu, num, pot)
+    return samples
+
+
+def pairs(start: float, cut: float, mu: float, species: Union[Collection[str], Tuple[str]], num: Optional[int] = 50,
+          pot: Tuple[float, float] = (5, 1), vacuum: Optional[float] = 20) -> Iterable[Atoms]:
+    samples = sample_pair_axes(start, cut, mu, num=num, pot=pot)
--- wmaee/extensions/unsupported/triplets/sampling.py
+++ wmaee/extensions/unsupported/triplets/sampling.py
@@ -9,7 +9,7 @@ from wmaee.core.types import Atoms
 ArrayLike = Union[np.ndarray, Collection]
 
 
-def sampling_function(x: ArrayLike, mu: float, num: int, pot: Tuple[float],
+def sampling_function(x: ArrayLike, mu: float, num: int, pot: Tuple[float, float],
                       indices: Optional[bool] = False) -> ArrayLike:
     """
     Calculates the asymmetric sampling with Mie-potential weights, around the equilibrium mu and returns the samples.
@@ -45,7 +45,8 @@ def sampling_function(x: ArrayLike, mu: float, num: int, pot: Tuple[float],
     return samples if not indices else np.array(si)
 
 
-def sample(start: float, cut: float, mu: float, num: Optional[int] = 50, pot: Tuple[float] = (5, 1)) -> np.ndarray:
+def sample_triangle(start: float, cut: float, mu: float, num: Optional[int] = 50,
+                    pot: Tuple[float, float] = (5, 1)) -> np.ndarray:
     """
     Samples a half circle of triplets, from a minimum distance to the a maximum distance of "cut" and denser
     sampling around "mu", where "pot" are the weights of a Mie-Type weighting function
@@ -82,53 +83,3 @@ def sample(start: float, cut: float, mu: float, num: Optional[int] = 50, pot: Tu
                     coords.append((rsample, ksample, ang))
                 num_samples += 1
     return np.array(coords).T
-
-
-def construct_triangle(rij: ArrayLike, rik: ArrayLike, theta: ArrayLike, origin: Tuple[float] = (0.0, 0.0, 0.0)) -> \
-        Tuple[np.ndarray]:
-    """
-    Constructs triangles, where first atom sits at origin, the second one along the x-axis and the third one
-    is sampled around in a half circle
-    :param rij: (np.ndarray) distance between atom 1 and 2
-    :param rik: (np.ndarray) distance between atom 1 and 3
-    :param theta: (np.ndarray) angle which is opposite of the hypothenuse
-    :param origin: (tuple of float) a shift of the origin
-    :return: (tuple np.ndarray) cordinates of atom 1, 2, and three
-    """
-    origin = np.array(origin)
-    first = np.zeros((3, len(rij)))
-    second = np.zeros((3, len(rij)))
-    third = np.zeros((3, len(rij)))
-    second[0, :] = rij
-    third[0, :] = rik * np.cos(theta)
-    third[1, :] = rik * np.sin(theta)
-    for i, coord in enumerate(origin):
-        first[i, :] += coord
-        second[i, :] += coord
-        third[i, :] += coord
-    return first, second, third
-
-
-def triangles(start: float, cut: float, mu: float, species: Union[Collection[str], Tuple[str]], num: Optional[int] = 50,
-              pot: Tuple[float] = (5, 1),
-              vacuum: Optional[float] = 20) -> Iterable[Atoms]:
-    """
-    :param species: (list of str) the species strings
-    :param start: (float) the minimum distance between the atoms
-    :param cut: (float) the cutoff radius
-    :param mu: (float) the expected minimum
-    :param num: (int) the number of samples on each axis
-    :param pot: (tuple of int) the powers of the Mie.Potential weighting function
-    :param vacuum: (float) how much vacuum will be added to pad from the next periodic image
-    :return:
-    """
-    grid = sample(start, cut, mu, num=num, pot=pot)
-    triangs = np.array(construct_triangle(*grid))
-    _, _, three = triangs
-    cell = (np.eye(3) * (np.amax(three, axis=1) - np.amin(three, axis=1)) + np.array([vacuum] * 3)) * np.eye(3)
-    pickle_file = 'grid.pickle'
-    if not exists(pickle_file):
-        with open(pickle_file, 'wb') as pickle_handle:
-            pickle.dump(grid, pickle_handle)
-    for i in range(triangs.shape[-1]):
-        yield Atoms(cell=cell, symbols=species, positions=triangs[:, :, i])
