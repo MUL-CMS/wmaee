7a52c30288e5f95ef5dbf9117ddd6ff087c5cfca
Dominik Nöger <dominik.noeger@unileoben.ac.at>
2023-06-13 08:53:55 +0200

    :memo: added and updated docstrings

--- wmaee/core/utils.py
+++ wmaee/core/utils.py
@@ -16,6 +16,7 @@ def override_environ(*remove, **update):
     is sure to work in all situations.
 
     :param remove: Environment variables to remove.
+    :type remove: str
     :param update: Dictionary of environment variables and values to add/update.
     """
     env = os.environ
@@ -47,9 +48,12 @@ class working_directory(object):
     def __init__(self, name=None, prefix=None, delete=False):
         """
         Constructs a working_directory object
-        :param name: (str) name of the directory if None is given os.getcwd() will be used (default: None)
-        :param prefix: (str) a prefix where to locate the directory (default: None)
-        :param delete: (bool) whether to delete the directory after a with clause (default: False)
+        :param name: name of the directory if None is given `os.getcwd()` will be used (default is `None`)
+        :type name: Optional[str]
+        :param prefix: a prefix where to locate the directory (default is `None`)
+        :type prefix: Optional[str]
+        :param delete: whether to delete the directory after a with clause (default is `False`)
+        :type delete: bool
         """
         self._name = str(uuid.uuid4()) if not name else name
         self._delete = delete
@@ -80,9 +84,42 @@ class working_directory(object):
         return self._active
 
 
-def merge(*dicts: Dict, factory: Type = dict, **kwargs) -> Dict:
+def merge(*dicts: Dict[Any, Any], factory: Type = dict, **kwargs) -> Dict[Any, Any]:
+    """
+    Merge all specified dictionaries in {dicts} into a single one. Moreover, {kwargs} will be included. In case
+    duplicate keys exist, the order of passing the dictionaries will determine which values will sustain for the keys.
+    The last updated is carried out using {kwargs}
+
+    :param dicts: the dictionaries to merge
+    :type dicts: Dict[Any, Any]
+    :param factory: the constructor of the mapping type to create (default is `dict`)
+    :type factory: Type
+    :return: a merged dictionary of type {factory}
+    :rtype: Dict[Any, Any]
+    """
+
     merged = factory()
     for dictionary in dicts:
         merged.update(dictionary)
     merged.update(kwargs)
     return merged
+
+
+def _wrap_in_iterable(o, factory=tuple):
+    return factory((o,))
+
+
+def ensure_iterable(o, exclude=(str, bytes), factory=tuple):
+    """
+    wraps an object {o} into an iterable it is not and iterable. the type of the iterable is specified by {factory}
+    :param o: the object to wrap
+    :param exclude: type list of iterable objects which need wrapping (default is (str, bytes))
+    :type exclude: Iterable[type]
+    :param factory: the Iterable type in which {o} should be wrapped
+    :type factory: type
+    :rtype: Iterable
+    """
+    if isinstance(o, collections.abc.Iterable):
+        return o if not isinstance(o, exclude) else _wrap_in_iterable(o, factory=factory)
+    else:
+        return _wrap_in_iterable(o, factory=factory)
