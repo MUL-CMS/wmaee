6491c62b44060b1144d62a3c31f02fbb3574f863
Dominik Nöger <dominik.noeger@unileoben.ac.at>
2023-06-15 09:54:01 +0200

    :recycle: refactor and forward imports

---
+++ wmaee/__init__.py
@@ -0,0 +1,6 @@
+
+
+from ase import Atoms
+from wmaee.core import GpawCalculation, VaspCalculation, MDCalculation, UnmetRequirement, show, show_traj
+
+__all__ = ["Atoms", "GpawCalculation", "VaspCalculation", "MDCalculation", "UnmetRequirement", "show", "show_traj"]
