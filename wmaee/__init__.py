9a5b124677fa84ea1ec4b382183b1905ee2331a6
Dominik Gehringer <35268573+dgehringer@users.noreply.github.com>
2023-06-16 13:03:43 +0200

    :recycle: forward imports

--- wmaee/__init__.py
+++ wmaee/__init__.py
@@ -1,6 +1,6 @@
-
-
 from ase import Atoms
-from wmaee.core import GpawCalculation, VaspCalculation, MDCalculation, UnmetRequirement, show, show_traj
+from wmaee.core import GpawCalculation, VaspCalculation, MDCalculation, UnmetRequirement, show, show_traj, \
+    available_models, available_lammps_models, md as md
 
-__all__ = ["Atoms", "GpawCalculation", "VaspCalculation", "MDCalculation", "UnmetRequirement", "show", "show_traj"]
+__all__ = ["Atoms", "GpawCalculation", "VaspCalculation", "MDCalculation", "UnmetRequirement", "show", "show_traj",
+           "available_models", "available_lammps_models", "md"]
