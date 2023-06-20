6303c85be867ea28e8a8062f41cee809f869519e
Dominik Gehringer <35268573+dgehringer@users.noreply.github.com>
2023-06-20 17:22:56 +0200

    :heavy_minus_sign: remove sympy dependency :recycle: refactored energy_volume.py

--- wmaee/extensions/energy_volume.py
+++ wmaee/extensions/energy_volume.py
@@ -5,11 +5,13 @@ Convenient functions for energy-volume calculations for the WMaE exercises.
 
 import os
 import numpy as np
+
 from ase import Atoms
 from os.path import join, exists
 from functools import partial
+from numpy.typing import ArrayLike
 from scipy.optimize import curve_fit
-from sympy import symbols, lambdify, S
+
 
 def energy_volume_curve(name: str, atoms: Atoms, deltas=None, incar=None, kpts=(1, 1, 1), show_output=False, ncpus=2,
                               xc="PBE"):
@@ -64,41 +66,30 @@ def energy_volume_curve(name: str, atoms: Atoms, deltas=None, incar=None, kpts=(
     #             data.append((deformed.lattice.volume, energy))
     #     return list(zip(*data))
 
-def birch_murnaghan_fit(volumes, energies, p0=None, return_analytic=False):
+
+def birch_murnaghan(volumes: ArrayLike, e0: float, v0: float, b0: float, bp: float):
+    two_thirds = 2/3
+    return e0 + (9 * v0 * b0 / 16) * (bp * ((volumes / v0) ** two_thirds - 1) ** 3 + ((volumes / v0) ** two_thirds - 1) ** 2 * (6 - 4 * (volumes / v0) ** two_thirds))
+
+
+def birch_murnaghan_fit(volumes, energies, p0=None):
     """
-    Performs a Birch-Murnaghan-fit on a given set of volues and energies
-    :param volumes: (iterable) volumes
-    :param energies (iterable) energies
+    Performs a Birch-Murnaghan-fit on a given set of volumes and energies
+    :param volumes: volumes
+    :type volumes: ArrayLike
+    :param energies: energies
+    :type energies: ArrayLike
     :param p0: (iterable) starting parameters in the order E0_guess, V0_guess, B0_guess, Bp_guess (default: None)
     :param return_analytic: (bool) wether also to return a SymPy analytic expression of the fit
     :returns: (optimum:np.array, E(V):callable, optional analytic:Sympy.Expression)
     """
 
-    # We define the variables at first
-    E0, V0, B0, V, Bp = symbols('E_0 V_0 B_0 V B_p')
-    E = E0 + (9 * V0 * B0 / 16) * (
-                Bp * ((V / V0) ** S('2/3') - 1) ** 3 + ((V / V0) ** S('2/3') - 1) ** 2 * (6 - 4 * (V / V0) ** S('2/3')))
-
-    equation_of_state = lambdify([V, E0, V0, B0, Bp], E, dummify=False)
     if p0 is None:
-        V0_guess = volumes[energies.index(min(energies))]
-        E0_guess = min(energies)
+        V0_guess = np.amin(volumes)
+        E0_guess = np.amin(energies)
         B0_guess = 100
         Bp_guess = 1
         p0 = (E0_guess, V0_guess, B0_guess, Bp_guess)
 
-    def eos(x, E0, V0, B0, Bp):
-        kwargs = dict(V=x, E_0=E0, V_0=V0, B_0=B0, B_p=Bp)
-        return equation_of_state(**kwargs)
-
-    optimum, residuals = curve_fit(eos, volumes, energies, p0=p0)
-    # Unpack the results
-    E0_value, V0_value, B0_value, Bp_value = optimum
-    # make a function E_(V)
-    E_ = partial(eos, E0=E0_value, V0=V0_value, B0=B0_value, Bp=Bp_value)
-    # before fitting we have to do a small trick
-    result = [optimum, E_]
-    if return_analytic:
-        result.append(E.subs(dict(E_0=E0_value, V_0=V0_value, B_0=B0_value, B_p=Bp_value)))
-
-    return tuple(result)
+    optimum, residuals = curve_fit(birch_murnaghan, np.array(volumes), np.array(energies), p0=p0)
+    return optimum, lambda volumes: birch_murnaghan(volumes, *optimum)
