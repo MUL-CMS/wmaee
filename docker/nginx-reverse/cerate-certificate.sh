005fe42d7740a18b6301f9f7ff05311c722def91
David Holec <david.holec@unileoben.ac.at>
2023-10-26 16:03:47 +0200

    removed docker from wmaee repo

--- docker/nginx-reverse/cerate-certificate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# this is a demo script do not generate a new certificate
-# You need a certificate that is signed by an authority. You have to apply for that at ZID via the CCDB app in Lotus notes
-# https://confluence.unileoben.ac.at:8443/pages/viewpage.action?pageId=36405321
-DOMAIN="modelling.unileoben.ac.at"
-
-openssl req -new -newkey rsa:4096 -nodes -keyout "$DOMAIN.key" -out "$DOMAIN.csr" -subj "/C=AT/ST=Steiermark/L=Leoben/O=Montanuniversität Leoben/OU=Department Materials Science/CN=$DOMAIN" -addext "subjectAltName = DNS:localhost,DNS:$DOMAIN,DNS:www.$DOMAIN"
-
