0b682458a739c525fb74711752292028aff394a6
David Holec <david.holec@unileoben.ac.at>
2023-11-10 22:11:36 +0100

    added env.yaml

--- wmaee/__init__.py
+++ wmaee/__init__.py
@@ -1,10 +1,10 @@
-import sys
+# import sys
 
-from ase import Atoms
-from ase.build import bulk
-from wmaee.core import GpawCalculation, VaspCalculation, MDCalculation, UnmetRequirement, show, show_traj, \
-    available_models, available_lammps_models, AbinitCalculation, md as md
+# from ase import Atoms
+# from ase.build import bulk
+# from wmaee.core import GpawCalculation, VaspCalculation, MDCalculation, UnmetRequirement, show, show_traj, \
+#     available_models, available_lammps_models, AbinitCalculation, md as md
 
 
-__all__ = ["Atoms", "GpawCalculation", "VaspCalculation", "MDCalculation", "UnmetRequirement", "show", "show_traj",
-           "available_models", "available_lammps_models", "md", "AbinitCalculation", "bulk"]
+# __all__ = ["Atoms", "GpawCalculation", "VaspCalculation", "MDCalculation", "UnmetRequirement", "show", "show_traj",
+#            "available_models", "available_lammps_models", "md", "AbinitCalculation", "bulk"]
