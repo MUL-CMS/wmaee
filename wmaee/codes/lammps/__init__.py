9977fd9781b2ad54fdfb578952a9738ac939a78c
David Holec <david.holec@unileoben.ac.at>
2023-11-22 15:12:03 +0100

    added docstrings for formwarded functions

--- wmaee/codes/lammps/__init__.py
+++ wmaee/codes/lammps/__init__.py
@@ -1,6 +1,81 @@
 from wmaee.codes.lammps.lammps_potential import get_models, get_potentials
 from wmaee.codes.lammps.lammps_runner import run_lammps
 from wmaee.codes.lammps.lammps_outputs import parse_logfile
-from ase.io.lammpsdata import write_lammps_data
+from ase.io.lammpsdata import write_lammps_data as _write_lammps_data
+from ase.io.lammpsrun import read_lammps_dump as _read_lammps_dump
+from typing import Union, IO, Any, List
+from ase import Atoms
 
-__all__ = [get_models, get_potentials, run_lammps, parse_logfile, write_lammps_data]
+def write_lammps_data(dumpfile: Union[str, IO], atoms: Atoms, specorder: Optional[List[str]] = None, 
+                      reduce_cell: bool = False, force_skew: bool = False, write_image_flags: bool = False,
+                      masses: bool = False, velocities: bool = False, units: str = 'metal', 
+                      atom_style: str = 'atomic'):
+    """
+    Write atomic structure data to a LAMMPS data file. Method forwarded from ase.io.lammpsdata.write_lammps_data.
+
+    Parameters
+    ----------
+    dumpfile : Union[str, IO]
+        File to which the output will be written.
+    atoms : Atoms
+        Atoms to be written.
+    specorder : Optional[List[str]], optional
+        Chemical symbols in the order of LAMMPS atom types, by default None
+    force_skew : bool, optional
+        Force to write the cell as a triclinic box (see LAMMPS documentation),
+        by default False
+    reduce_cell : bool, optional
+        Whether the cell shape is reduced or not, by default False
+    write_image_flags : bool, default False
+        If True, the image flags indicating in which images of the periodic
+        simulation box the atoms are, are written.
+    masses : bool, optional
+        Whether the atomic masses are written or not, by default False
+    velocities : bool, optional
+        Whether the atomic velocities are written or not, by default False
+    units : str, optional
+        LAMMPS units (see LAMMPS documentation), by default 'metal'
+    atom_style : {'atomic', 'charge', 'full'}, optional
+        LAMMPS atom style (see LAMMPS documentation), by default 'atomic'
+    """
+    _write_lammps_data(dumpfile, atoms, specorder=specorder, reduce_cell=reduce_cell, force_skew=force_skew, 
+                       write_image_flags=write_image_flags, masses=masses, velocities=velocities, units=units,
+                       atom_style=atom_style)
+    
+
+def read_lammps_dump(dump_file: Union[str, IO], index: Any = None, **kwargs) -> Union[Atoms, List[Atoms]]:
+    """
+    Read Atoms object(s) from a LAMMPS dump file. Method forwarded from ase.io.lammpsrun.read_lammps_dump.
+
+    Parameters
+    ----------
+    dump_file : Union[str, IO]
+        Name of the file to read from or a file descriptor.
+
+    index : Any, optional
+        Specifies which configuration to read. 
+        Default is None, which returns the last configuration.
+        
+        Examples
+        --------
+        index=0
+            First configuration
+        index=-2
+            Second to last
+        index=':'
+            All configurations
+        index='-3:'
+            Three last configurations
+        index='::2'
+            Even configurations
+        index='1::2'
+            Odd configurations
+
+    Returns
+    -------
+    Union[Atoms, List[Atoms]]
+        An Atoms object or a list of Atoms objects depending on the number of configurations read.
+    """
+    return _read_lammps_dump(dump_file, index=index)
+
+__all__ = [get_models, get_potentials, run_lammps, parse_logfile, write_lammps_data, read_lammps_dump]
--- wmaee/codes/lammps/lammps_outputs.py
+++ wmaee/codes/lammps/lammps_outputs.py
@@ -1,24 +1,44 @@
 # Modified from: https://github.com/henriasv/lammps-logfile/tree/master/lammps_logfile
 import pandas as pd
-import numpy as np
-from io import BytesIO, StringIO
+from io import StringIO
+from typing import Union, List, Dict, Optional
 
-def parse_logfile(logfile, return_df=True):
+def parse_logfile(logfile: str, return_df: bool = True) -> Union[pd.DataFrame, List[Dict[str, List[Optional[float]]]]]:
+    """
+    Parse a LAMMPS logfile and extract thermo data.
+
+    Parameters
+    ----------
+    logfile : str
+        Path to the LAMMPS logfile.
+    return_df : bool, optional
+        If True, returns a pandas DataFrame; if False, returns a list of dictionaries,
+        by default True.
+
+    Returns
+    -------
+    Union[pd.DataFrame, List[Dict[str, List[Optional[float]]]]]
+        Parsed thermo data either as a pandas DataFrame or a list of dictionaries.
+    """
+    # Strings that indicate the start and stop of thermo data in the logfile
     start_thermo_strings = ['Memory usage per processor', 'Per MPI rank memory allocation']
     stop_thermo_strings = ['Loop time', 'ERROR']
-    partial_logs = []
     
+    # List to store parsed thermo data
+    partial_logs = []
+
     with open(logfile, 'r') as log:
         contents = log.readlines()
+
     keyword_flag = False
     i = 0
+
     while i < len(contents):
         line = contents[i]
 
         if keyword_flag:
-            # keywords = line.split()
             tmpString = ''
-            # Check wheter any of the thermo stop strigs are in the present line
+            # Check whether any of the thermo stop strings are in the present line
             while not sum([string in line for string in stop_thermo_strings]) >= 1:
                 if "\n" in line:
                     tmpString += line
@@ -27,27 +47,25 @@ def parse_logfile(logfile, return_df=True):
                     line = contents[i]
                 else:
                     break
+
+            # Parse the thermo data using pandas
             partialLogContents = pd.read_table(StringIO(tmpString), sep=r'\s+')
 
-            # if (self.keywords != keywords):
-            #     # If the log keyword changes, i.e. the thermo data to be outputted chages,
-            #     # we flush all prevous log data. This is a limitation of this implementation. 
-            #     self.flush_dict_and_set_new_keyword(keywords)
-
-            partial_dict = {}
-            # for name in keywords:
-            #     # data_dict[name] = np.append(data_dict[name],partialLogContents[name])
-            #     partial_dict[name] = list(partialLogContents[name].values)#np.append(np.asarray([]), partialLogContents[name])
             if return_df:
                 partial_logs.append(partialLogContents)
             else:
-                partial_logs.append(partialLogContents.to_dict(orient = 'list'))
+                # Convert DataFrame to a list of dictionaries if return_df is False
+                partial_logs.append(partialLogContents.to_dict(orient='list'))
+
             keyword_flag = False
 
         # Check whether the string matches any of the start string identifiers
         if sum([line.startswith(string) for string in start_thermo_strings]) >= 1:
             keyword_flag = True
+
         i += 1
+
+    # Return either a single DataFrame or a list of dictionaries
     if len(partial_logs) == 1:
         return partial_logs[0]
     return partial_logs
