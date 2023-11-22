f26bddcf569aefb3d58d7a185e6f49509ab5f611
David Holec <david.holec@unileoben.ac.at>
2023-11-22 16:00:15 +0100

    updated function definitions

--- wmaee/codes/lammps/__init__.py
+++ wmaee/codes/lammps/__init__.py
@@ -3,13 +3,12 @@ from wmaee.codes.lammps.lammps_runner import run_lammps
 from wmaee.codes.lammps.lammps_outputs import parse_logfile
 from ase.io.lammpsdata import write_lammps_data as _write_lammps_data
 from ase.io.lammpsrun import read_lammps_dump as _read_lammps_dump
-from typing import Union, IO, Any, List
+from typing import Union, IO, Any, List, Optional
 from ase import Atoms
 
 def write_lammps_data(dumpfile: Union[str, IO], atoms: Atoms, specorder: Optional[List[str]] = None, 
-                      reduce_cell: bool = False, force_skew: bool = False, write_image_flags: bool = False,
-                      masses: bool = False, velocities: bool = False, units: str = 'metal', 
-                      atom_style: str = 'atomic'):
+                      force_skew: bool = False, velocities: bool = False, units: str = 'metal', 
+                      atom_style: str = 'atomic', **kwargs):
     """
     Write atomic structure data to a LAMMPS data file. Method forwarded from ase.io.lammpsdata.write_lammps_data.
 
@@ -24,26 +23,16 @@ def write_lammps_data(dumpfile: Union[str, IO], atoms: Atoms, specorder: Optiona
     force_skew : bool, optional
         Force to write the cell as a triclinic box (see LAMMPS documentation),
         by default False
-    reduce_cell : bool, optional
-        Whether the cell shape is reduced or not, by default False
-    write_image_flags : bool, default False
-        If True, the image flags indicating in which images of the periodic
-        simulation box the atoms are, are written.
-    masses : bool, optional
-        Whether the atomic masses are written or not, by default False
-    velocities : bool, optional
-        Whether the atomic velocities are written or not, by default False
     units : str, optional
         LAMMPS units (see LAMMPS documentation), by default 'metal'
     atom_style : {'atomic', 'charge', 'full'}, optional
         LAMMPS atom style (see LAMMPS documentation), by default 'atomic'
     """
-    _write_lammps_data(dumpfile, atoms, specorder=specorder, reduce_cell=reduce_cell, force_skew=force_skew, 
-                       write_image_flags=write_image_flags, masses=masses, velocities=velocities, units=units,
-                       atom_style=atom_style)
+    _write_lammps_data(dumpfile, atoms, specorder=specorder, force_skew=force_skew,
+                       velocities=velocities, units=units, atom_style=atom_style, **kwargs)
     
 
-def read_lammps_dump(dump_file: Union[str, IO], index: Any = None, **kwargs) -> Union[Atoms, List[Atoms]]:
+def read_lammps_dump(dump_file: Union[str, IO], index: Any = slice(None), **kwargs) -> Union[Atoms, List[Atoms]]:
     """
     Read Atoms object(s) from a LAMMPS dump file. Method forwarded from ase.io.lammpsrun.read_lammps_dump.
 
@@ -54,7 +43,7 @@ def read_lammps_dump(dump_file: Union[str, IO], index: Any = None, **kwargs) -> 
 
     index : Any, optional
         Specifies which configuration to read. 
-        Default is None, which returns the last configuration.
+        Default is slice(None), which returns all configurations.
         
         Examples
         --------
@@ -62,7 +51,7 @@ def read_lammps_dump(dump_file: Union[str, IO], index: Any = None, **kwargs) -> 
             First configuration
         index=-2
             Second to last
-        index=':'
+        index=slice(None)
             All configurations
         index='-3:'
             Three last configurations
