099057761097b0f320acb7c9d53aed60859101d0
David Holec <david.holec@unileoben.ac.at>
2024-08-14 17:53:42 +0200

    removed buggy __init__.py from pyiron

--- wmaee/codes/pyiron/__init__.py
+++ wmaee/codes/pyiron/__init__.py
@@ -1,4 +1,4 @@
-from wmaee.codes.pyiron.pyiron_NEB_task import NEB, NEBjob, _wrap_positions
-from wmaee.codes.pyiron.pyiron_cluster import assign_partition_mulhpc
+#from wmaee.codes.pyiron.pyiron_NEB_task import NEB, NEBjob, _wrap_positions
+#from wmaee.codes.pyiron.pyiron_cluster import assign_partition_mulhpc
 
-__all__ = ['NEB', 'NEBjob', '_wrap_positions', 'assign_partition_mulhpc']
+#__all__ = ['NEB', 'NEBjob', '_wrap_positions', 'assign_partition_mulhpc']
