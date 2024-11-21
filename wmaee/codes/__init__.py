aa78367178670ae69cc7e5277560b2e755fd55ee
David Holec <david.holec@unileoben.ac.at>
2024-11-21 08:57:41 +0100

    pmg patch for parsing Vasprun is not needed anymore

--- wmaee/codes/__init__.py
+++ wmaee/codes/__init__.py
@@ -1,5 +1,7 @@
 # apply home-made patches
 # see folder patches for details
-from wmaee.core.requirements import test_pmg
-if test_pmg():
-    import wmaee.codes.patches.pmg_vasprun
+
+# not needed as of 2024-11-21
+#from wmaee.core.requirements import test_pmg
+#if test_pmg():
+#    import wmaee.codes.patches.pmg_vasprun
